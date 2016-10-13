78a0c26333cc346e7e06d28a2fd887ef915df6b5
Barry Howard <BarryLeeHoward@gmail.com>
2016-10-12 21:37:38 -0400

    Hard mode without title and author

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-
+// Easy Mode
+// -------------------------------
 
 currentOpen = null;
 
@@ -10,7 +11,6 @@ function switchOpen(nOpen){
 	}
 
 }
-
 
 function toggleSize(event){
 	var target = $(event.target);
@@ -25,3 +25,47 @@ function toggleSize(event){
 }
 
 $(".header").click(toggleSize);
+
+
+// Hard Mode
+// ----------------------------
+
+var currentPic = 1;
+var outputPic = $(".image-Background");
+outputPic.toggleClass("pic1");
+
+function changePic(event){
+	var target = $(event.target);
+	var direction = target.attr('id');
+	var previousPic = currentPic;
+	currentPic = newPic(currentPic, direction);
+	var picUrl = carouselImages[currentPic-1].image_url;
+	var picTitle = carouselImages[currentPic-1].title;
+	var picId = carouselImages[currentPic-1].photographer_id;
+	console.log(currentPic);
+	console.log(picUrl);
+	var outputPic = $(".image-Background");
+	var curPicClass = "pic" + currentPic;
+	var prevPicClass = "pic" + previousPic;
+	outputPic.toggleClass(curPicClass);
+	outputPic.toggleClass(prevPicClass);
+	console.log(curPicClass);
+	console.log(prevPicClass);
+	console.log(outputPic);
+
+
+}
+
+function newPic(curPic, direction){
+	if (direction === "right" && curPic<7){
+		return curPic + 1;
+	} else if(direction ==="right"){
+		return 1;
+	} else if(direction ==="left" && curPic>1){
+		return curPic -1;
+	} else if(direction === "left"){
+		return 7;
+	}
+}
+
+$(".scrollButton").click(changePic);
